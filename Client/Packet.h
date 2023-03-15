14c726cc72b76868b205df158ee1a4e4fa2fb84f
Jose Ibanez Polo <Jibanezpolo3629@conestogac.on.ca>
2023-03-15 11:12:30 -0400

    Commit #1 (Jose) - Packet structure defined.

--- Client/Packet.h
+++ Client/Packet.h
@@ -1,1 +1,31 @@
 #pragma once
+#include <ctime>
+
+class Packet {
+
+private:
+	struct Header
+	{
+		char sourceID;
+		char destinationID;
+		unsigned int sequenceNumber;
+		time_t timestamp;
+		bool ackFlag : 1;
+		bool errFlag : 1;
+		bool finFlag : 1;
+		unsigned int bodyLength;
+	}head;
+
+	struct Body
+	{
+		char* username;
+		char* response;
+	}body;
+
+	char* pSerialBuf;
+	unsigned int crc;
+
+public:
+
+
+};
