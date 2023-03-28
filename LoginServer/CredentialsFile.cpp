b534a50990eb98e0b26ef66f4c56514ae2dcef7a
MateoVG-coding <mateovega10@hotmail.com>
2023-03-28 18:47:21 -0400

    Add CredentialFiles functions.

---
+++ LoginServer/CredentialsFile.cpp
@@ -0,0 +1,53 @@
+#include "CredentialsFile.h"
+
+void addCredentials(string username, string password, string filename) {
+    
+    ofstream outFile(filename, ios::app); 
+
+    if (outFile.is_open()) 
+    {
+        outFile << username << ":" << password << endl;
+        outFile.close();
+    }
+    else 
+    {
+        cout << "Error opening credentials file!" << endl;
+    }
+}
+
+void createCredentialsFile(string filename) 
+{  
+    ifstream file(filename);
+
+    if (file.good()) // check if the file already exists
+    {
+        cout << "Credentials file already exists" << endl;
+        file.close();
+        return;
+    }
+    
+    file.close();
+
+    ofstream outFile(filename); // create the file if doesn't exist
+    if (outFile.is_open()) 
+    {
+        cout << "Credentials file created" << endl;
+        outFile.close(); 
+    }
+    else 
+    {
+        cout << "Error creating credentials file!" << endl;
+    }
+}
+
+void deleteCredentialsFile(string filename) 
+{
+    int result = remove(filename.c_str());
+
+    if (result == 0) {
+        cout << "Credentials file deleted" << endl;
+    }
+    else {
+        cout << "Error deleting credentials file!" << endl;
+    }
+}
