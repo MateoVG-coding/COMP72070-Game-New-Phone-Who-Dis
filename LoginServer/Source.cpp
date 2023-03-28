6ec381dff7d60459b56208c8d5b4a225a26a2b95
MateoVG-coding <mateovega10@hotmail.com>
2023-03-28 18:24:13 -0400

    Add main and clientHandler function.

--- LoginServer/Source.cpp
+++ LoginServer/Source.cpp
@@ -155,6 +155,7 @@ int main(int argc, char* argv[])
 
     while (true)
     {
+
         // accept incoming connection
         SOCKET clientSocket;
         sockaddr_in clientAddr;
@@ -167,6 +168,7 @@ int main(int argc, char* argv[])
 
         thread t(clientHandler, clientSocket);
         clientThreads.push_back(move(t));
+
     }
 
     for (auto& t : clientThreads) {
